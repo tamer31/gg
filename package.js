bddc681e82d6a38e898a0f7d62aeec8a7d1dfbb1
tamer31 <94556087+tamer31@users.noreply.github.com>
2025-10-31 03:12:25 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "telegram-board",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "type": "module",
+  "dependencies": {
+    "node-telegram-bot-api": "^0.63.0",
+    "express": "^4.18.2",
+    "fs-extra": "^11.2.0"
+  }
+}
